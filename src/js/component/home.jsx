8e607537d02dbae67bfac6914a190d3e874e339e
EScott237 <143569907+EScott237@users.noreply.github.com>
2023-11-12 05:15:23 +0000

    Erika's second update

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,36 +1,44 @@
 import React, { useState } from "react";
 
-//create your first component
 const Home = () => {
-	const [ inputValue, setInputValue ] = useState("");
-	const [ todos, setToDos ] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setToDos] = useState([]);
 
-	 
-
-
-	return (
-		<div className="container">
-			<h1>todos</h1>
-			<ul>
-				<li>
-					<input type="text" 
-						onChange={(e) => setInputValue(e.target.value)}
-						value={inputValue} 
-						onKeyPress={(e) => {
-							if (e.key === "Enter") {
-								setToDos(todos.concat(inputValue));
-								setInputValue("");
-							}
-						}}
-						placeholder="What needs to be done?"></input>
-				</li>
-				{todos.map((item, index) => (
-					<li>{item}{" "}<i className="fas fa-times" onClick={() => setToDos(todos.filter((t, currentIndex) => index != currentIndex))}></i></li>
-				))}
-			</ul>
-			<div>{todos.length} Items Left</div>
-		</div>
-	);
+  return (
+    <div className="container">
+      <h1>todos</h1>
+      <ul>
+        <li>
+          <input
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+                setToDos(todos.concat(inputValue));
+                setInputValue("");
+              }
+            }}
+            placeholder="What needs to be done?"
+          />
+        </li>
+        {todos.length === 0 ? (
+          <li>No tasks, add a task</li>
+        ) : (
+          todos.map((item, index) => (
+            <li key={index}>
+              {item}{" "}
+              <i
+                className="fas fa-times"
+                onClick={() => setToDos(todos.filter((t, currentIndex) => index !== currentIndex))}
+              ></i>
+            </li>
+          ))
+        )}
+      </ul>
+      <div>{todos.length} Items Left</div>
+    </div>
+  );
 };
 
 export default Home;
